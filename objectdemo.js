f2a2c77ce851152256a87fe0a27dfe67fba6ecb1
Rajnish Mishra <rm924581@gmail.com>
2026-04-01 23:17:55 +0530

    Create objectdemo.js

---
+++ objectdemo.js
@@ -0,0 +1,16 @@
+let product = {
+    name:"Apple Iphone 11 pro ma(512 GB, Blue)",
+    price: 150000,
+    discount: 15,
+    isAvailable: false
+};
+
+let socialHandles = {
+    fb:{x : 10},
+    instagram: 67255,
+    "link": 200,
+    x: "null"
+};
+
+console.log(socialHandles); 
+
