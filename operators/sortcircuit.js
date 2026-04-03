d864c8f2e7b19f8bd9f975a0f30c82f55ce94b67
Rajnish Mishra <rm924581@gmail.com>
2026-04-03 20:27:14 +0530

    Create sortcircuit.js

---
+++ operators/sortcircuit.js
@@ -0,0 +1,10 @@
+console.log(10 && 30);  //30
+console.log(0 && 30);   //0
+console.log(40 && null); //null
+console.log("" && -0); //" "
+
+
+console.log(10 || 30); //10
+console.log(0 || 30); //30
+console.log(40 || null); //40
+console.log("" || -0); //-0 
