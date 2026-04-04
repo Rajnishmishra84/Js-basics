42a059a5faeff167117d2e61ce7c6bdc7b053d7c
Rajnish Mishra <rm924581@gmail.com>
2026-04-04 20:51:00 +0530

    Create elseifdemo.js

---
+++ conditional/elseifdemo.js
@@ -0,0 +1,13 @@
+let userSubscription = "unsubscribed";
+
+if(userSubscription == "unsubscribed"){
+    console.log("Show pricing page");
+} else if (userSubscription == "super") {
+    console.log("Only hindi content");
+} else if(userSubscription == "premium"){
+    console.log("Show all content");
+} else{
+    console.log("Show happy")
+}
+
+console.log("End....");
