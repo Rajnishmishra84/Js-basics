53d6e6ac6d7fc8bc2b525e213efeb6fa8a7b0740
Rajnish Mishra <rm924581@gmail.com>
2026-04-04 20:50:54 +0530

    Create ifdemo.js

---
+++ conditional/ifdemo.js
@@ -0,0 +1,16 @@
+console.log("Starting....");
+
+let isUserSignedIn = false;
+let isPaidUser = true;
+
+if (isUserSignedIn == true && isPaidUser == true) {
+    // this particular block will only execute if the condition mentioned is true...
+    console.log("show the dashboard");
+} else{
+    console.log("Show the home page");
+}
+
+console.log("End.....");
+
+
+
