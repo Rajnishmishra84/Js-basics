389f3cf4a57ae952b4e6fe40ba1357aaa495d969
Rajnish Mishra <rm924581@gmail.com>
2026-04-04 20:51:04 +0530

    Create nestedifelseemo.js

---
+++ conditional/nestedifelseemo.js
@@ -0,0 +1,13 @@
+let isUserSubscribed = true;
+let userHasLionsGatePlay = false;
+let userSony = true;
+
+if(isUserSubscribed) {
+    if(userHasLionsGatePlay) {
+        console.log("Watch john wick");
+    } else if(userSony) {
+        console.log("Show the Guardian");
+    } else {
+        console.log("Show panchaayat")
+    } 
+}
