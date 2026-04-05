84116bc2f3de421cd98c83a17e6dcc8cef16673f
Rajnish Mishra <rm924581@gmail.com>
2026-04-05 13:57:01 +0530

    Create bitwiseoperators.js

---
+++ operators/bitwiseoperators.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 6;
+
+console.log(a & b); // bitwise and
+
+console.log(a | b); // bitwise or
+
+console.log(5 ^ 6); // bitwise xor
+
+console.log(1 << 3); //left shift (2^x)
+
+console.log(27 >> 2); //right shift (x/2 -> floor)
