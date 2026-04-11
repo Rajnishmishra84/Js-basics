055a20f3f8517b78fe6481d58e599e327db5e19e
Rajnish Mishra <rm924581@gmail.com>
2026-04-11 09:09:28 +0530

    Create function2.js

---
+++ function/function2.js
@@ -0,0 +1,11 @@
+function isEvenOrOdd(x) {
+    // x ---> parameter --> placeholder --> expected input
+    if(x % 2 == 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+isEvenOrOdd(10); // --> arguments --> actual input
+
