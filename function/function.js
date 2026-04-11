e82b0f0669cd87b264131377187f062b11783540
Rajnish Mishra <rm924581@gmail.com>
2026-04-11 09:09:25 +0530

    Create function.js

---
+++ function/function.js
@@ -0,0 +1,9 @@
+function greetingToEveryone() {
+    console.log("my logic line 1");
+    console.log("my logic line 2");
+    console.log("my logic line 3");
+}
+
+greetingToEveryone();    
+
+console.log(Math.sqrt(100));
